006afd9f92e0317e461c68b20d5a5260e168b379
vienpham2019 <vienpham2019@gmail.com>
2020-03-22 13:49:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+
+class Shape {
+    constructor(side_arr){
+        this.side_arr = side_arr
+    }
+
+    get countSides(){
+        return this.side_arr.length
+    }
+
+    get perimeter(){
+        return this.side_arr.reduce((sum,e) => sum + e, 0)
+    }
+}
+
+class Polygon extends Shape{
+}
+
+class Triangle extends Shape{
+    get isValid(){
+        let arr = [...this.side_arr]
+        let c = Math.max(...this.side_arr)
+        arr.splice(arr.indexOf(c),1)
+        return arr[0] + arr[1] > c ? true : false
+    }
+}
+
+class Square extends Shape{
+    get area(){
+        return this.side_arr[0] ** 2
+    }
+
+    get isValid() {
+        return this.side_arr.filter((e,i) => this.side_arr.indexOf(e) === i).length === 1 ? true : false 
+    }
+}
